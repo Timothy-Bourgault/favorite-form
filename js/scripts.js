15473b8b6bd0059412b4b9006f76136851fc2ff7
Joshua Casuccio and Tim Bourgault <joshua@>
2016-08-15 11:20:40 -0700

    add unordered list from array

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,17 @@ $(document).ready(function() {
     formData[4]=sexPosition;
     formData[5]=portlandBar;
     // formData.push() = [name, food, location, musicGenre, sexPosition, portlandBar]
-    updateData.push(formData[1],formData[0],formData[2]);
+    updateData.push(formData[0], formData[1], formData[2], formData[3], formData[4], formData[5]);
     console.log(formData);
     event.preventDefault();
+    $("ul").append("<li>" + updateData.shift(0) + "</li>");
+    $("ul").append("<li>" + updateData.shift(1) + "</li>");
+    $("ul").append("<li>" + updateData.shift(2) + "</li>");
+    $("ul").append("<li>" + updateData.shift(3) + "</li>");
+    $("ul").append("<li>" + updateData.shift(4) + "</li>");
+    $("ul").append("<li>" + updateData.shift(5) + "</li>");
+    // updateData.forEach(function(updateData) {
+    //   item = "<li>" + updateData + "</li>";
+    //   $("outPut").prepend(item);
   });
 });
